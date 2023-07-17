5354c5d2695aebbe30aa03273b07119aa0233763
ZoutigeWolf <guuskamphuis@gmail.com>
2023-07-17 12:58:37 +0200

    Initial Commit

---
+++ Jonko Tracker/Jonko_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jonko_TrackerApp.swift
+//  Jonko Tracker
+//
+//  Created by ZoutigeWolf on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Jonko_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
